b801c545a76e4c73b646bce9655fe67900563bb3
Chris <87771733+ChrisCh4padia@users.noreply.github.com>
2025-07-16 19:33:49 +0200

    adjust everything

---
+++ DVDS-SUPPLYCHAIN-CHALLENGE/ground-control-station/launch_qgc.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+if [ "x${HEADLESS}" == "1" ]; then
+    echo -e "INFO\t[QGC] HEADLESS SET. RUNNING IN HEADLESS MODE."
+    xvfb-run /home/gcs/QGroundControl.AppImage
+else
+    /home/gcs/QGroundControl.AppImage
+fi
